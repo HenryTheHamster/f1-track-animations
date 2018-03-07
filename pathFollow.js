27198983ce08235565cb4784ee93a41561528e01
Ryan Boucher <ryan.boucher@distributedlife.com>
2018-03-07 16:25:23 +1100

    Now there are two cars; and it's more pure

--- pathFollow.js
+++ pathFollow.js
@@ -7,11 +7,8 @@ const lerp = (v1, v2, t) => v1 * (1 - t) + (v2 * t);
 const normalise = (t, b) => (t - b) / (1 - b)
 
 const linear = (t) => t;
-
 const easeInQuad = (t) => t * t;
-
 const easeOut = (p) => (t) => 1 - Math.abs(Math.pow(t - 1, p));
-
 const eastOutQuad = easeOut(2)
 const eastOutCubic = easeOut(3)
 const eastOutQuart = easeOut(4)
@@ -22,76 +19,57 @@ const v = (t) => t / 10;
 
 const Easings = {
   linear: linear,
-  corner: linear,
-  straight: (t) => lerp(linear(t), eastOutCubic(t), t),
+  corner: (t) => lerp(easeInQuad(t), linear(t), t),
+  straight: (t) => lerp(linear(t), eastOutQuad(t), t),
 }
-const SupportedEasings = Object.keys(Easings);
 
+const SupportedEasings = Object.keys(Easings);
 const easyLikeSundayMorning = (func) => (t) => Math.max(t, clamp(Easings[func](t)));
 
 
-function ready(error, xml) {
+const pathStartPoint = (path) => path.attr("d").split(/[\sA]+/)[1].split(",")
+const translateAlong = (path) => {
+  const l = path.getTotalLength();
 
-  //Adding our svg file to HTML document
+  return (i) => (t) => {
+    const p = path.getPointAtLength((isNaN(t) ? 1 : t) * l);
+
+    return `translate(${p.x},${p.y})`;
+  }
+}
+
+function transition(marker, speed, paths, i) {
+  const thisPath = i >= paths.length ? 0 : i;
+  const path = paths[thisPath]
+
+  const time = path.getTotalLength() * speed;
+
+  const classes = path.getAttribute('class').split(' ');
+  const easings = classes.filter((classname) => SupportedEasings.includes(classname));
+  const easing = easings.length === 0 ? 'linear' : easings[0];
+
+  marker.transition()
+      .duration(time)
+      .ease(easyLikeSundayMorning(easing))
+      .attrTween("transform", translateAlong(path))
+      .each("end", () => transition(marker, speed, paths, thisPath + 1));
+}
+
+function ready(error, xml) {
   const importedNode = document.importNode(xml.documentElement, true);
   d3.select("#pathAnimation").node().appendChild(importedNode);
 
   const svg = d3.select("svg");
   const tracks = svg.selectAll("path.track");
   const track = svg.select("path.track");
-  console.log(tracks);
-  console.log(track);
-  console.log(track.attr("d"));
   const startPoint = pathStartPoint(tracks);
 
-  const marker = svg.append("circle");
-  marker.attr("r", 7)
-    .attr("transform", "translate(" + startPoint + ")");
+  const marker1 = svg.append("circle");
+  marker1.attr("r", 5).attr("transform", `translate(${startPoint})`)
 
-  transition(tracks[0], 0);
+  const marker2 = svg.append("rect");
+  marker2.attr("width", 5).attr("height", 5).attr("transform", `translate(${startPoint})`)
 
-  //Get path start point for placing marker
-  function pathStartPoint(path) {
-    const d = path.attr("d"),
-    dsplitted = d.split(/[\sA]+/);
-    return dsplitted[1].split(",");
-  }
-
-  function transition(paths, i) {
-    if (i >= paths.length) {
-      i = 0;
-    }
-
-    const speed = 20;
-    const time = paths[i].getTotalLength() * speed;
-
-    const classes = paths[i].getAttribute('class').split(' ');
-    const easings = classes.filter((classname) => SupportedEasings.includes(classname));
-    const easing = easings.length === 0 ? 'linear' : easings[0];
-
-    marker.transition()
-        .duration(time)
-        .ease(easyLikeSundayMorning(easing))
-        .attrTween("transform", translateAlong(paths[i]))
-        .each("end", function() { return transition(paths, i + 1) });// infinite loop
-  }
-
-
-// NEED CUSTOM EASING FUNCTION
-
-
-  function translateAlong(path) {
-    const l = path.getTotalLength();
-    return function(i) {
-      return function(t) {
-        if(isNaN(t)) {
-          t = 1;
-        }
-
-        const p = path.getPointAtLength(t * l);
-
-        return "translate(" + p.x + "," + p.y + ")";//Move marker
-      }
-    }
-  }
+  transition(marker1, 5, tracks[0], 0);
+  transition(marker2, 5.25, tracks[0], 1);
 }
